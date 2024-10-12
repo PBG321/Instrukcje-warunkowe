91f00c6f80b20a5e583f8b8cd02e46da2c1620d4
PBG321 <pawelbigos48@gmail.com>
2024-10-12 14:52:02 +0200

    Hello!

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("Hello world!!!")
+
+imie = input('Wprowadz imie')
+nazwisko = input('Wprowadz nazwisko')
+
+print(f"Witaj {imie} {nazwisko}. Dzisiaj poznasz tajemnice instrukcji warunkowych!")
